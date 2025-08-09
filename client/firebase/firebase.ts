193f378914a960d6a9bce834b8853f0e746c462d
Shaymaa-aldossary <shaymaaahadossry@gmail.com>
2025-08-10 00:07:53 +0300

    Update firebase config to use env variables

--- client/firebase/firebase.ts
+++ client/firebase/firebase.ts
@@ -6,13 +6,14 @@ import { getFirestore } from "firebase/firestore";
 
 // ✅ بيانات مشروعك من Firebase Console 
 const firebaseConfig = {
-  apiKey: "AIzaSyBy-98A_rSsEe8xX8jcSFBVJgxC4WjeCak",
-  authDomain: "wathiq-9761e.firebaseapp.com",
-  projectId: "wathiq-9761e",
-  storageBucket: "wathiq-9761e.firebasestorage.app",
-  messagingSenderId: "159174686365",
-  appId: "1:159174686365:web:d65f16e564f5cd344dc674",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // ✅ تهيئة التطبيق
 const app = initializeApp(firebaseConfig);
