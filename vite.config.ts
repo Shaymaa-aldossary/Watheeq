8f9bf3bea75e5dc16de2cdd3c47d00f9b4fe85f5
Shaymaa-aldossary <shaymaaahadossry@gmail.com>
2025-08-10 00:23:41 +0300

    Add vercel.json for routing fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  
   server: {
     host: "::",
     port: 8080,
@@ -23,6 +24,7 @@ export default defineConfig(({ mode }) => ({
       "@shared": path.resolve(__dirname, "./shared"),
     },
   },
+  
 }));
 
 function expressPlugin(): Plugin {
